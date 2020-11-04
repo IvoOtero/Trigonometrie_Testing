02f202c9916ee00405f310eb53de90939f3b8ef5
Ivo Otero <ivo.tomas4@hotmail.com>
2020-11-04 23:15:46 +0100

    Projektdateien hinzufügen.

---
+++ PerformTrigonometricFuntionsSteps.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist.ValueRetrievers;
+using Xunit;
+
+namespace Trigonometrie_Testing
+{
+
+    
+
+    [Binding]
+    public class PerformTrigonometricFuntionsSteps
+    {
+
+        public string function;
+
+        [Given(@"I choose the (.*) function in my calculator")]
+        public void GivenIChooseTheFunctionInMyCalculator(string p0)
+        {
+            function = p0;
+            Assert.NotNull(p0);
+        }
+        
+        [Given(@"the angular degree of the function is (.*)")]
+        public void GivenTheAngularDegreeOfTheFunctionIs(double pi)
+        {
+            double expected = Math.PI;
+            double actual = pi;
+            Assert.Equal(expected, actual, 5);
+        }
+        
+        [When(@"I press the (.*) button")]
+        public void WhenIPressTheButton(string button)
+        {
+            var expected = "<=>";
+            var actual = button;
+            Assert.Equal(expected, actual);
+        }
+        
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(int result)
+        {
+            if (function == "<Sine>" || function == "<Tangent>")
+            {
+                var expected = 0;
+                var actual = result;
+                Assert.Equal(expected, actual);
+            } 
+            else if (function == "<Cosine>")
+            {
+                var expected = -1;
+                var actual = result;
+                Assert.Equal(expected, actual);
+            }
+        }
+    }
+}
---
+++ SpecFlowFeature1.feature
@@ -0,0 +1,23 @@
+﻿Feature: Perform trigonometric funtions
+	As User
+	I want to carry out trigonometic functions on my calculator
+	So that I don´t have to calculate them myself.
+
+@IvoOtero
+Scenario: Sine
+	Given I choose the <Sine> function in my calculator
+	And the angular degree of the function is 3.14159265359
+	When I press the <=> button 
+	Then the result should be 0
+
+Scenario: Cosine
+	Given I choose the <Cosine> function in my calculator
+	And the angular degree of the function is 3.14159265359
+	When I press the <=> button 
+	Then the result should be -1
+
+Scenario: Tangent
+	Given I choose the <Tangent> function in my calculator
+	And the angular degree of the function is 3.14159265359
+	When I press the <=> button 
+	Then the result should be 0
